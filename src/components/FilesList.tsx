b49d142331318ff53732aa3473b8fb467b017235
Karol Stawowski <stawowski.dev@gmail.com>
2023-07-08 20:45:30 +0200

    chore: improve files list

--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -5,15 +5,23 @@ type FilesListProps = {
 }
 
 export const FilesList = ({ files }: FilesListProps) => {
-  console.log(files)
   return (
     <div className="w-full px-6 py-6 bg-slate-300 rounded-lg">
       <h2 className="font-medium text-lg">Files to compress</h2>
       <div className="h-[2px] w-8 bg-violet-500 mb-4 mt-1" />
+
       {files ? (
-        <ul className="divide-y">
+        <ul className="flex flex-col gap-1">
           {files.map((file) => (
-            <li>{file.name}</li>
+            <div>
+              <li className="py-1 px-2 bg-violet-300 text-violet-900 border-violet-500 border text-sm ">
+                {file.name}
+              </li>
+              <div class="bg-gray-400 w-full h-[2px] relative overflow-hidden z-10">
+                <div class="absolute w-full top-0 left-0 h-full bg-violet-600 animate-fill-to-right"></div>
+              </div>
+              {/* <div className="bg-violet-600 h-[2px] w-full" /> */}
+            </div>
           ))}
         </ul>
       ) : (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fill-to-right': 'fillLeftToRight 1.2s ease-in-out forwards',
+      },
+      keyframes: {
+        fillLeftToRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
