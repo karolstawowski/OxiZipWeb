7b82940f8c23d9b43af2e037d77703f8ce781901
Karol Stawowski <stawowski.dev@gmail.com>
2023-07-09 17:19:28 +0200

    fix: display github logo

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,11 @@
+import GithubImage from '../images/Github.svg'
+
 export const Heading = () => {
   return (
     <div className="w-full fixed top-0 left-0 h-12 bg-violet-300 flex items-center px-8 md:px-10 xl:px-12 justify-between">
       <h1 class="text-xl font-bold text-violet-950 tracking-wide">OxiZipWeb</h1>
       <a href="https://github.com/karolstawowski/OxiZipWeb">
-        <img src="src/icons/Github.svg" width={28} alt="github repository" />
+        <img src={GithubImage} width={28} alt="github repository" />
       </a>
     </div>
   )
