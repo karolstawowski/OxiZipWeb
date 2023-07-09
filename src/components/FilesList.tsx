aed0212bbf84b8b0a81bce3117c0a41de7c15f14
Karol Stawowski <stawowski.dev@gmail.com>
2023-07-09 14:49:24 +0200

    chore: add file removal

--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -1,27 +1,40 @@
 import { FileOff } from '../icons/FileOff'
+import { X } from '../icons/X'
+import { byteFormatter } from '../lib/units'
 
 type FilesListProps = {
   files: File[] | null
+  handleFileRemoval: (fileName: string) => void
 }
 
-export const FilesList = ({ files }: FilesListProps) => {
+export const FilesList = ({ files, handleFileRemoval }: FilesListProps) => {
   return (
     <div className="w-full px-6 py-6 bg-slate-300 rounded-lg">
       <h2 className="font-medium text-lg">Files to compress</h2>
       <div className="h-[2px] w-8 bg-violet-500 mb-4 mt-1" />
 
-      {files ? (
+      {files && files.length > 0 ? (
         <ul className="flex flex-col gap-1">
           {files.map((file) => (
-            <div>
-              <li className="py-1 px-2 bg-violet-300 text-violet-900 border-violet-500 border text-sm ">
-                {file.name}
-              </li>
-              <div class="bg-gray-400 w-full h-[2px] relative overflow-hidden z-10">
-                <div class="absolute w-full top-0 left-0 h-full bg-violet-600 animate-fill-to-right"></div>
+            <li>
+              <div className="py-1 px-2 bg-violet-300 text-violet-900 border-violet-500 border text-sm flex justify-end items-center">
+                <span className="mr-auto">{file.name}</span>
+                <div className="flex gap-4 items-center">
+                  <span>{byteFormatter.format(file.size)}</span>
+                  <button
+                    onClick={() => handleFileRemoval(file.name)}
+                    className="flex items-center"
+                  >
+                    <span className="text-violet-900 font-bold hover:bg-violet-500 bg-violet-400 inline-block rounded-sm border border-violet-900">
+                      <X size={20} />
+                    </span>
+                  </button>
+                </div>
               </div>
-              {/* <div className="bg-violet-600 h-[2px] w-full" /> */}
-            </div>
+              <div class="bg-gray-400 w-full h-[2px] relative overflow-hidden z-10">
+                <div class="absolute w-full top-0 left-0 h-full bg-violet-600 animate-fill-to-right" />
+              </div>
+            </li>
           ))}
         </ul>
       ) : (
---
+++ src/icons/X.tsx
@@ -0,0 +1,20 @@
+export const X = ({ size = 24 }: { size?: number }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-x"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M18 6l-12 12"></path>
+      <path d="M6 6l12 12"></path>
+    </svg>
+  )
+}
